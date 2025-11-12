dc91e7dcf528d7e9509c624263a71161fc7e7e24
CristianLechon <calechon@uce.edu.ec>
2025-11-11 20:04:09 -0500

    Tarea 3 Calculadora

--- js/calculadoraFinal.js
+++ js/calculadoraFinal.js
@@ -14,11 +14,39 @@ function limpiarDisplay() {
 
 function borrarDisplay() {
     let elemento = document.getElementById('display');
-    elemento.innerText = valor;
+    elemento.innerText = elemento.innerText.slice(0, -1);
 }
 
-function sumar(num1, num2) {
-    resultado = num1 + num2;
+function calcularResultado() {
+    let elemento = document.getElementById('display');
+    // Usamos expresión regular para capturar num1, operador, num2 [signo opcional][número][operador][signo opcional][número]
+    let operadores = elemento.innerText.match(/^(-?\d+(?:\.\d+)?)([+\-*/%])(-?\d+(?:\.\d+)?)$/);
+    // Extraemos del Array
+    num1 = parseFloat(operadores[1]);
+    let operador = operadores[2];
+    num2 = parseFloat(operadores[3]);
+    // dividimos la cadena en partes mediante el operador
+    let res = 0;
+    if (operador === '+') {
+        res = sumar(parseFloat(num1), parseFloat(num2));
+    }
+    if (operador === '-') {
+        res = restar(parseFloat(num1), parseFloat(num2));
+    }
+    if (operador === '*') {
+        res = multiplicar(parseFloat(num1), parseFloat(num2));
+    }
+    if (operador === '/') {
+        res = dividir(parseFloat(num1), parseFloat(num2));
+    }
+    if (operador === '%') {
+        res = porcentaje(parseFloat(num1), parseFloat(num2));  
+    }
+    elemento.innerText = res;
+}
+
+function sumar(valor1, valor2) {
+    resultado = valor1 + valor2;
     return resultado;
 }
 function restar(num1, num2) {
@@ -32,10 +60,14 @@ function multiplicar(num1, num2) {
 
 }
 function dividir(num1, num2) {
+    if (num2 === 0){
+        return 'Error';
+    }
     resultado = num1 / num2;
     return resultado;
 }
 
-function porcentaje() {
-
+function porcentaje(num1, num2) {
+    resultado = (num1 * num2) / 100;
+    return resultado;
 }
